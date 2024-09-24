1cbf2b81b7a1b0cc5b966ea2f88a7df611b1da6a
Alok Kumar Rawat <149565273+AlokKumarRawat@users.noreply.github.com>
2024-09-24 22:20:31 +0530

    Create Question8.cpp

---
+++ Question8.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int a,b,c,d,e;
+  cout<<"Enter the marks of five subject: "<<"\n";
+  cin>>a;
+  cin>>b;
+  cin>>c;
+  cin>>d;
+  cin>>e;
+  cout<<"Percentage is "<<(a+b+c+d+e)/5;
+  
+  return 0;
+}
