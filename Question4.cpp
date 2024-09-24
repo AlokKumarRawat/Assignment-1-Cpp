07aeb5bbc00b0fa8921b062e050fc27323cc1ddc
Alok Kumar Rawat <149565273+AlokKumarRawat@users.noreply.github.com>
2024-09-24 21:57:26 +0530

    Create Question4.cpp

---
+++ Question4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+  float a,b,c,d,e;
+  cout<<"Enter the temperature of five cities of Gujarat:\n";
+  cin>>a;
+  cin>>b;
+  cin>>c;
+  cin>>d;
+  cin>>e;
+  
+  cout<<"The average temperatue is is "<<(a+b+c+d+e)/5;
+ 
+  return 0;
+}
