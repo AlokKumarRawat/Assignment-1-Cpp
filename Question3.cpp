b65e25045c35cc3e16db38063d0b57134ba8155c
Alok Kumar Rawat <149565273+AlokKumarRawat@users.noreply.github.com>
2024-09-24 21:53:16 +0530

    Create Question3.cpp

---
+++ Question3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int a,b,c;
+  cin>>a;
+  cin>>b;
+  cin>>c;
+  
+  cout<<"The total is "<<(a+b+c)<<"\n";
+  cout<<"The average is "<<(a+b+c)/3;
+ 
+  return 0;
+}
